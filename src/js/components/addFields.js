089e84ba3660800ec9918af5c1c8b4dedb32e7fb
don bencilao <don bencilao>
2016-08-30 21:07:25 +0800

    use react bootstrap modal

--- src/js/components/addFields.js
+++ src/js/components/addFields.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import {Modal} from 'react-bootstrap';
+import {Modal,Button} from 'react-bootstrap';
 
 
 export default class AddFields extends React.Component{
-
+    hide(){
+        this.setState({showModal:false});
+    }
     render(){
         return (
-            <div className="static-modal">
-                <Modal.Dialog>
-                <Modal.Header>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
+            <div className="static-modal" show={this.state.showModal} >
+                <Modal.Dialog >
+                    <Modal.Header>
+                        <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
 
-                <Modal.Body>
-                    One fine body...
-                </Modal.Body>
+                    <Modal.Body>
+                        One fine body...
+                    </Modal.Body>
 
-                <Modal.Footer>
-                    <Button>Close</Button>
-                    <Button bsStyle="primary">Save changes</Button>
-                </Modal.Footer>
+                    <Modal.Footer>
+                        <Button>Close</Button>
+                        <Button bsStyle="primary">Save changes</Button>
+                    </Modal.Footer>
 
                 </Modal.Dialog>
             </div>
--- src/js/pages/createform.js
+++ src/js/pages/createform.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {FormGroup, FormControl,Modal,Button} from 'react-bootstrap';
-import {AddFields} from '../components/addFields';
+import RU from 'react-utils-helper/lib';
+import AddFields from '../components/addFields';
 
 export default class CreateForm extends React.Component{
     
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {
+            showModal:false
+        }
+        
     }
 
     openAddFieldsModal(e){
         e.preventDefault();
-       // console.log(<AddFields />);
+       // this.state.showModal = !this.state.showModal;
+        
+        console.log(this.state);
       //  return (<AddFields />);
         
     }
 
+    close(){
+        this.state.showModal = false;
+    }
+
     render(){
+        
         return (
             <div className="createForm">
                 <form>
@@ -24,9 +36,13 @@ export default class CreateForm extends React.Component{
                     <FormGroup>
                         <FormControl type="text" placeholder="Form title" />
                     </FormGroup>
-                    <button class="btn btn-default" onClick={this.openAddFieldsModal.bind(this)}>Add Field</button>
+                     <Button  bsStyle="primary"
+                        bsSize="large"
+                        onClick={this.openAddFieldsModal.bind(this)}>open modal
+                    </Button>
                 </form>
-                <div id="addfieldsModal"></div>
+                //use react-utils here, to show and hide something
+               
             </div>
             );
     }
