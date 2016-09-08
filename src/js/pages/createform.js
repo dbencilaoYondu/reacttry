d7d04a3356b13b8c996f641e71d8d52d6f716328
Don Bencilao <Don Bencilao@egg.ph>
2016-09-08 16:44:57 +0800

    changed state rendering to immutable way

--- src/js/pages/createform.js
+++ src/js/pages/createform.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {FormGroup, FormControl,Modal,Button, ControlLabel } from 'react-bootstrap';
+import {FormGroup, FormControl,Modal,Button, ControlLabel, Checkbox } from 'react-bootstrap';
 import Field from '../components/fields';
 
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {addField} from '../actions/addField'
 
-let fields = []
+
 class CreateForm extends React.Component{
     
     constructor(props){
@@ -29,33 +29,82 @@ class CreateForm extends React.Component{
         this.setState({fieldName:event.target.value})
         console.log(event.target.value)
     }
-    addElement(e){
+    addElement(e){ 
         e.preventDefault()
-        console.log(this.state.fieldType)
-        
-        fields.push({
-            fieldType:this.state.fieldType,
-            fieldName:this.state.fieldName
+        //  let fields = this.state.fields.slice();
+        // console.log(this.state.fieldType)
+        // fields.push({
+        //     fieldType:this.state.fieldType,
+        //     fieldName:this.state.fieldName
+        // })
+        //console.log(fields)
+        this.setState({fields:this.state.fields.concat({
+                fieldType:this.state.fieldType,
+                fieldName:this.state.fieldName
+            })
         })
-        console.log(fields)
-        this.setState({fields:fields})
 
             console.log(this.state)
             console.log(this.props)
     }
     showFormElements(){
         return this.state.fields.map(function(field,index){
-           return <FormGroup key={index}>
+
+           switch(field.fieldType){
+               case 'short text box':
+                  return  <FormGroup key={index}>
                         <ControlLabel>{field.fieldName}</ControlLabel>
                         <FormControl type="text"/>
                   </FormGroup>
+               break;
+               case 'long text box':
+                  return <FormGroup controlId="formControlsTextarea">
+                        <ControlLabel>{field.fieldName}</ControlLabel>
+                        <FormControl componentClass="textarea" placeholder="textarea" />
+                </FormGroup>
+               break;
+               case 'selection':
+                  return  <FormGroup controlId="formControlsSelect">
+                        <ControlLabel>{field.fieldName}</ControlLabel>
+                        <FormControl componentClass="select" placeholder="select">
+                            <option value="select">select</option>
+                            <option value="other">...</option>
+                        </FormControl>
+                    </FormGroup>
+               break;
+               case 'checkbox':
+                  return   <FormGroup>
+                        <Checkbox inline>
+                            1
+                        </Checkbox>
+                        {' '}
+                        <Checkbox inline>
+                            2
+                        </Checkbox>
+                        {' '}
+                        <Checkbox inline>
+                            3
+                        </Checkbox>
+                    </FormGroup>
+                break;
+                case 'product list':
+                  return  <div>
+                        products here
+                    </div>
+                break;
+                case 'static text':
+                  return  <div>
+                        static text here
+                    </div>
+                break;
+           }
         })
     }
     render(){
            
         return (
             <div className="createForm row">
-                <div className="left createForm col-sm-6">
+                <div className="left createForm col-sm-4">
                     <h1>Create Form</h1>
                     <form onSubmit={this.addElement.bind(this)}>
                         <FormGroup controlId="formControlsSelect">
@@ -80,8 +129,8 @@ class CreateForm extends React.Component{
                         <button class="btn" type="submit">add field</button>
                     </form>
                 </div>
-                <div className="right viewForm col-sm-6">
-                    {this.showFormElements()}
+                <div className="right viewForm col-sm-6 col-sm-offset-1">
+                {this.state.fields.length > 0 ? this.showFormElements() : <h4 class="text-center addFieldHere">Add fields here...</h4>}
                 </div>
                 
             </div>
